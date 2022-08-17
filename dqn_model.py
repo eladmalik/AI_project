c66670f36ac971d02a42d09761f458a35a5d3034
tsachim <elad.malik@mail.huji.ac.il>
2022-08-17 14:04:12 +0300

    added dqnagent3

--- dqn_model.py
+++ dqn_model.py
@@ -59,3 +59,31 @@ class DQNAgent2(DQNAgent):
 
     def forward(self, x):
         return self.network(x)
+
+
+class DQNAgent3(DQNAgent):
+    def __init__(self, input_size, output_size, hidden_size1, hidden_size2, hidden_size3, hidden_size4, hidden_size5):
+        super().__init__(int(input_size), int(output_size))
+        input_size = int(input_size)
+        hidden_size1 = int(hidden_size1)
+        hidden_size2 = int(hidden_size2)
+        hidden_size3 = int(hidden_size3)
+        hidden_size4 = int(hidden_size4)
+        hidden_size5 = int(hidden_size5)
+        output_size = int(output_size)
+
+        self.time_created = utils.get_time()
+        self.network = nn.Sequential(nn.Linear(input_size, hidden_size1),
+                                     nn.ReLU(),
+                                     nn.Linear(hidden_size1, hidden_size2),
+                                     nn.ReLU(),
+                                     nn.Linear(hidden_size2, hidden_size3),
+                                     nn.ReLU(),
+                                     nn.Linear(hidden_size3, hidden_size4),
+                                     nn.ReLU(),
+                                     nn.Linear(hidden_size4, hidden_size5),
+                                     nn.ReLU(),
+                                     nn.Linear(hidden_size5, output_size))
+
+    def forward(self, x):
+        return self.network(x)
