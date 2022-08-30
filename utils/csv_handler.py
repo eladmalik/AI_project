265bb2f662dc424bef98daed039da40d4b5e271b
malik <elad.malik@mail.huji.ac.il>
2022-08-30 13:09:22 +0300

    added dqn2

--- utils/csv_handler.py
+++ utils/csv_handler.py
@@ -2,20 +2,20 @@ import csv
 import os.path
 from typing import List, Any, Dict
 
-from utils.enums import DataType
+from utils.enums import StatsType
 
 CSV_FILE = "results.csv"
 
 
 class csv_handler:
-    def __init__(self, folder: str, parameters: List[DataType]):
+    def __init__(self, folder: str, parameters: List[StatsType]):
         self.folder = folder
         self.out_file = open(os.path.join(self.folder, CSV_FILE), 'w', encoding="utf-8", newline='')
         self.writer = csv.writer(self.out_file, delimiter=",", quoting=csv.QUOTE_NONE)
         self.parameters = parameters
         self.writer.writerow([param.value for param in self.parameters])
 
-    def write_row(self, values_dict: Dict[DataType, Any]):
+    def write_row(self, values_dict: Dict[StatsType, Any]):
         """
         writes a row to the csv file
         """
